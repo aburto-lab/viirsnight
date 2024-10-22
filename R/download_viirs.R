07c986d1ca1eb533d7ed5c62d76ca88168800eb5
Fabio Favoretto <Favoretto.fabio@gmail.com>
2024-10-22 20:34:37 +0200

    Update download_viirs.R
    years parameters is now a vector

--- R/download_viirs.R
+++ R/download_viirs.R
@@ -11,10 +11,10 @@
 #' @examples
 #' download_viirs(2019)
 #'
-download_viirs <- function(year, destination_folder, username, password) {
+download_viirs <- function(years, destination_folder, username, password) {
 
-  if (missing(year)) {stop("A year from 2013 to 2021 must be selected")}
-  if (!year %in% 2013:2021) {stop("A year from 2013 to 2021 must be selected")}
+  if (missing(years)) {stop("At least one year from 2013 to 2021 must be selected")}
+  if (any(!years %in% 2013:2021)) {stop("All years must be between 2013 and 2021")}
   if (missing(destination_folder)) {stop("No destination folder is selected")}
   if (!dir.exists(destination_folder)) {
     dir.create(destination_folder)
@@ -31,40 +31,37 @@ download_viirs <- function(year, destination_folder, username, password) {
     grant_type = 'password'
   )
 
-  if(year == 2013) {
-    data_url <-
-    paste0(
-      "https://eogdata.mines.edu/nighttime_light/annual/v21/",
-      year,
-      "/VNL_v21_npp_",
-      year,
-      "_global_vcmcfg_c202205302300.average_masked.dat.tif.gz"
-    )
-    } else {
-      data_url <-
-        paste0(
-          "https://eogdata.mines.edu/nighttime_light/annual/v21/",
-          year,
-          "/VNL_v21_npp_",
-          year,
-          "_global_vcmslcfg_c202205302300.average_masked.dat.tif.gz"
-        )
-    }
-
-  token_url <-
-    'https://eogauth.mines.edu/auth/realms/master/protocol/openid-connect/token'
+  token_url <- 'https://eogauth.mines.edu/auth/realms/master/protocol/openid-connect/token'
   response <- httr::POST(token_url, body = params, encode = "form")
-  access_token_list <-
-    jsonlite::fromJSON(httr::content(response, as = "text", encoding = "UTF-8"))
+  access_token_list <- jsonlite::fromJSON(httr::content(response, as = "text", encoding = "UTF-8"))
   access_token <- access_token_list$access_token
-
   auth <- paste('Bearer', access_token)
 
-  output_file <- paste0(destination_folder, "/", basename(data_url))
   options(timeout=36000)
-  download.file(url = data_url,
-                destfile = output_file,
-                mode = "wb",
-                headers = list(Authorization = auth))
 
+  for (year in years) {
+    if (year == 2013) {
+      data_url <- paste0(
+        "https://eogdata.mines.edu/nighttime_light/annual/v21/",
+        year,
+        "/VNL_v21_npp_",
+        year,
+        "_global_vcmcfg_c202205302300.average_masked.dat.tif.gz"
+      )
+    } else {
+      data_url <- paste0(
+        "https://eogdata.mines.edu/nighttime_light/annual/v21/",
+        year,
+        "/VNL_v21_npp_",
+        year,
+        "_global_vcmslcfg_c202205302300.average_masked.dat.tif.gz"
+      )
+    }
+
+    output_file <- paste0(destination_folder, "/", basename(data_url))
+    download.file(url = data_url,
+                  destfile = output_file,
+                  mode = "wb",
+                  headers = list(Authorization = auth))
+  }
 }
